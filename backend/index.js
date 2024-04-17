c08c1f060ea4600801326d0279cbc3a2ff2b6883
Rohan <rohan.2021b0131077@abes.ac.in>
2024-04-17 17:42:42 +0530

    deployment ready

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import http from 'http';
 import cors from 'cors';
 import dotenv from 'dotenv';
-
+import path from 'path';
 import passport from 'passport';
 import session from 'express-session';
 import connectMongoDBSession from 'connect-mongodb-session';
@@ -19,7 +19,7 @@ import { configurePassport } from './passport/passport.config.js';
 
 dotenv.config();
 configurePassport();
-
+const __dirname= path.resolve();
 const app = express();
 const httpServer = http.createServer(app);
 
@@ -75,6 +75,12 @@ app.use(
   }),
 );
 
+// npm run build will build your frontend app, it will be the optimized version of your app
+app.use(express.static(path.join(__dirname, "frontend/dist")))
+
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname, "frontend/dist","index.html"));
+})
 // Modified server startup
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
 await connectDB();
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,7 +9,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client
 
 const client = new ApolloClient({
   // PRODUCTION M DALNE SE PHLE UPDATE KRDENA URI
-  uri: "http://localhost:4000/graphql",  //URL of our graphQL server
+  uri: import.meta.env.VITE_NODE_ENV ==="development" ? "http://localhost:4000/graphql": "/graphql",  //URL of our graphQL server
   cache: new InMemoryCache(), //apollo client uses this to cache query results after fetching them
   credentials:"include", //this tells apollo client to send cookies along with every request to the server
 });
